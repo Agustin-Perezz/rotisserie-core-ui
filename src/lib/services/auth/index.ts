6c3f9e5de466802b567e94818bdcf6dd081915d3
Agustin-Perezz <agustinnicolas806@gmail.com>
2025-08-07 15:23:25 -0300

    Add auth service

---
+++ src/lib/services/auth/index.ts
@@ -0,0 +1,57 @@
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  FacebookAuthProvider,
+  type AuthError,
+  type UserCredential
+} from 'firebase/auth';
+import { firebaseApp } from '$lib/firebase';
+import { errorToast, successToast } from '$lib/alerts/toast';
+import {
+  CREDENTIALS_NOT_FOUND,
+  SUCCESS_SIGN_IN_MESSAGE
+} from '../constanst/auth.constants';
+
+const auth = getAuth(firebaseApp);
+
+const provider = new GoogleAuthProvider();
+const facebookProvider = new FacebookAuthProvider();
+
+export const signInWithGoogle = async (): Promise<UserCredential | null> => {
+  try {
+    const result = await signInWithPopup(auth, provider);
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+
+    if (!credential) {
+      throw new Error(CREDENTIALS_NOT_FOUND);
+    }
+
+    //const token = credential.accessToken;
+    successToast(SUCCESS_SIGN_IN_MESSAGE);
+    return result;
+  } catch (error) {
+    const firebaseError = error as AuthError;
+    errorToast(firebaseError.message);
+    return null;
+  }
+};
+
+export const signInWithFacebook = async (): Promise<UserCredential | null> => {
+  try {
+    const result = await signInWithPopup(auth, facebookProvider);
+    const credential = FacebookAuthProvider.credentialFromResult(result);
+
+    if (!credential) {
+      throw new Error(CREDENTIALS_NOT_FOUND);
+    }
+
+    //const token = credential.accessToken;
+    successToast(SUCCESS_SIGN_IN_MESSAGE);
+    return result;
+  } catch (error) {
+    const firebaseError = error as AuthError;
+    errorToast(firebaseError.message);
+    return null;
+  }
+};
