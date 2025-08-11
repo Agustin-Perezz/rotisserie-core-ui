89cecffc4001281b1fcfd3da8fdf1fddc02ddf7c
Agustin-Perezz <agustinnicolas806@gmail.com>
2025-08-11 08:41:56 -0300

    Add shop service

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
   import ShopForm from '$lib/components/shop/ShopForm.svelte';
+  import { createShop } from '$lib/services/shop';
 
   type Test = {
     name: string;
-    description: string;
+    description?: string;
     location: string;
   };
 
-  const handleSubmit = (values: Test) => {
-    console.log('Form submitted:', values);
+  const handleSubmit = async (values: Test) => {
+    const test = await createShop(values);
+    console.log('Shop created:', test);
   };
 </script>
 
