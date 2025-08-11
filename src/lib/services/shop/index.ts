aef123585751d97c2329aa630adbd9106158f525
Agustin-Perezz <agustinnicolas806@gmail.com>
2025-08-11 09:18:27 -0300

    Update props object in axios post request

--- src/lib/services/shop/index.ts
+++ src/lib/services/shop/index.ts
@@ -3,9 +3,7 @@ import api from '$lib/axios';
 import type { TShop, TShopFormData } from '$lib/types/shop';
 
 export const createShop = async (shopData: TShopFormData): Promise<TShop> => {
-  const response = await api.post<TShop>('/shops', {
-    ...shopData
-  });
+  const response = await api.post<TShop>('/shops', shopData);
   successToast('Negocio creado exitosamente!');
   return response.data;
 };
