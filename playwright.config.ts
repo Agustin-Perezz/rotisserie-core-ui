ad35ed114f467f2b85de9c861807d50c6cdbf0a8
Agustin-Perezz <agustinnicolas806@gmail.com>
2025-08-12 09:21:15 -0300

    Add env to the test

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,13 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   webServer: {
     command: 'npm run build && npm run preview',
-    port: 4173
+    port: 4173,
+    env: {
+      VITE_FIREBASE_API_KEY: process.env.VITE_FIREBASE_API_KEY ?? '',
+      VITE_FIREBASE_AUTH_DOMAIN: process.env.VITE_FIREBASE_AUTH_DOMAIN ?? '',
+      VITE_FIREBASE_PROJECT_ID: process.env.VITE_FIREBASE_PROJECT_ID ?? '',
+      VITE_FIREBASE_APP_ID: process.env.VITE_FIREBASE_APP_ID ?? ''
+    }
   },
   testDir: 'e2e'
 });
