36b3e8b88f095758eff32067c4848abea5a69d7b
Agustin-Perezz <agustinnicolas806@gmail.com>
2025-08-12 11:00:38 -0300

    Add navigate button to create a new shop

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,5 +4,7 @@
 
 <div class="flex h-screen flex-col items-center justify-center">
   <h1 class="mb-4 text-2xl font-bold">Hola!, como andas ?</h1>
-  <Button href="/shop/new">Crea un nuevo negocio</Button>
+  <Button href="/shop/new" data-testid="navigate-shop-button"
+    >Crea un nuevo negocio</Button
+  >
 </div>
