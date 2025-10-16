13368241c58d6582781ab3dedb179f188032f0ab
Agustin-Perezz <agustinnicolas806@gmail.com>
2025-10-16 08:18:06 -0300

    Add object to format data util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -10,6 +10,28 @@ export function calculateTotalPrice(items: TOrderItemContext[]): number {
   return items.reduce((total, item) => total + item.price * item.quantity, 0);
 }
 
+export function objectToFormData(obj: Record<string, unknown>): FormData {
+  const formData = new FormData();
+
+  Object.entries(obj).forEach(([key, value]) => {
+    if (value === undefined || value === null) {
+      return;
+    }
+
+    if (value instanceof File) {
+      formData.append(key, value);
+    } else if (typeof value === 'number') {
+      formData.append(key, value.toString());
+    } else if (typeof value === 'string') {
+      formData.append(key, value);
+    } else {
+      formData.append(key, String(value));
+    }
+  });
+
+  return formData;
+}
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type WithoutChild<T> = T extends { child?: any } ? Omit<T, 'child'> : T;
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
