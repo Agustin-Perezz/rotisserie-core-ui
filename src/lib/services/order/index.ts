7342e3ce96c3a752644b288e0d8e7b056a946b2c
Agustin-Perezz <agustinnicolas806@gmail.com>
2025-10-21 08:18:29 -0300

    Add logic to update order status

--- src/lib/services/order/index.ts
+++ src/lib/services/order/index.ts
@@ -1,5 +1,9 @@
 import api from '$lib/axios';
-import type { TCreateOrderRequest, TOrder } from '$lib/types/order';
+import type {
+  TCreateOrderRequest,
+  TOrder,
+  TOrderStatus
+} from '$lib/types/order';
 
 export const createOrder = async (
   data: TCreateOrderRequest
@@ -13,5 +17,13 @@ export const createOrder = async (
   };
 
   const response = await api.post('/orders', orderData);
+  return response.data;
+};
+
+export const updateOrderStatus = async (
+  orderId: string,
+  status: TOrderStatus
+): Promise<TOrder> => {
+  const response = await api.patch(`/orders/${orderId}`, { status });
   return response.data;
 };
