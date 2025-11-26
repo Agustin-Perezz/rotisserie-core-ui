516c6f0c89df673fdcd1a4577e698e5f4707ff07
Agustin-Perezz <agustinnicolas806@gmail.com>
2025-11-26 09:54:53 -0300

    Create dashboard breadcrumb component for shop navigation

---
+++ src/lib/components/ui/shop/DashboardBreadcrumb.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+  import { goto } from '$app/navigation';
+  import * as Breadcrumb from '$lib/components/ui/breadcrumb';
+
+  interface Props {
+    shopId: string;
+    shopName: string;
+    currentPage?: string;
+  }
+
+  let { shopId, shopName, currentPage }: Props = $props();
+</script>
+
+<Breadcrumb.Root class="my-4 flex justify-center">
+  <Breadcrumb.List>
+    <Breadcrumb.Item>
+      <Breadcrumb.Link
+        href="/shops"
+        onclick={(e) => {
+          e.preventDefault();
+          goto('/shops');
+        }}
+      >
+        Shops
+      </Breadcrumb.Link>
+    </Breadcrumb.Item>
+    <Breadcrumb.Separator />
+    <Breadcrumb.Item>
+      <Breadcrumb.Link
+        href={`/shop/${shopId}/dashboard`}
+        onclick={(e) => {
+          e.preventDefault();
+          goto(`/shop/${shopId}/dashboard`);
+        }}
+      >
+        {shopName}
+      </Breadcrumb.Link>
+    </Breadcrumb.Item>
+    <Breadcrumb.Separator />
+    <Breadcrumb.Item>
+      {#if currentPage}
+        <Breadcrumb.Link
+          href={`/shop/${shopId}/dashboard`}
+          onclick={(e) => {
+            e.preventDefault();
+            goto(`/shop/${shopId}/dashboard`);
+          }}
+        >
+          Dashboard
+        </Breadcrumb.Link>
+      {:else}
+        <Breadcrumb.Page>Dashboard</Breadcrumb.Page>
+      {/if}
+    </Breadcrumb.Item>
+    {#if currentPage}
+      <Breadcrumb.Separator />
+      <Breadcrumb.Item>
+        <Breadcrumb.Page>{currentPage}</Breadcrumb.Page>
+      </Breadcrumb.Item>
+    {/if}
+  </Breadcrumb.List>
+</Breadcrumb.Root>
